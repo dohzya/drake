a5f632b39d289284c5b05cc6d29136516f6d48b0
Julien Richard-Foy <julien@richard-foy.fr>
2014-06-27 14:56:24 +0200

    update client-side router generation

--- app/assets/javascripts/drake.js
+++ app/assets/javascripts/drake.js
@@ -4,18 +4,28 @@ define(['abyssa-with-deps', 'qajax'], function (abyssa, qajax) {
 
     qajax.getJSON("/router").then(function (routes) {
       var router = abyssa.Router();
-      routes.forEach(function (route) {
-        var pattern = route.path.reduce(function (acc, part) {
-          if (typeof part === 'string') return acc + part;
-          else return acc + ':' + part.name; // TODO handle regexp constraints
-        }, '');
-        var state = abyssa.State(pattern, function () {
-          if (!router.isFirstTransition()) {
-            targets[route.call.controller][route.call.method].apply(null, arguments)
+      var processRules = function (rules, prefix) {
+        rules.forEach(function (rule) {
+          if (rule.type === 'route') {
+            var pattern = rule.path.reduce(function (acc, part) {
+              if (typeof part === 'string') return acc + part;
+              else return acc + ':' + part.name; // TODO handle regexp constraints
+            }, prefix);
+            var state = abyssa.State(pattern, function () {
+              if (!router.isFirstTransition()) {
+                targets[rule.call.controller][rule.call.method].apply(null, arguments)
+              }
+            });
+            router.addState(rule.call.controller + '_' + rule.call.method, state);
+          } else /* rule.type === 'router' */ {
+            processRules(rule.rules, prefix + rule.prefix);
           }
         });
-        router.addState(route.call.controller + '_' + route.call.method, state);
-      });
+      };
+
+      // I assume that the root object is always a router with prefix '/'
+      processRules(routes.rules, '');
+
       router.init();
     });
 
